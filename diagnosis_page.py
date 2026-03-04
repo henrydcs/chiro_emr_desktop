5ab8471da388a3a3edff7de204a75522084c439f
Henry Guerena <henrydcs@gmail.com>
2026-03-04 12:46:29 -0800

    diagnose disc cs ls updated

--- diagnosis_page.py
+++ diagnosis_page.py
@@ -54,7 +54,7 @@ DX_LIST: list[tuple[str, str]] = [
     ("----------------------------------", "-----------------------------------"),
 
     # Cervical
-    ("Cervical disc displacement", "M50.20"),
+    ("Herniated disc, Cervical Spine", "M50.20"),
     ("Cervical sprain/strain (whiplash)", "S13.4XXA"),  
     ("Radiculopathy, Cervical Region", "M54.12"),
     ("Cervical muscle spasm", "M62.838"),
@@ -65,7 +65,7 @@ DX_LIST: list[tuple[str, str]] = [
     ("-----------------------------------", "-------------------------------"),
 
     # Thoracic
-    ("Thoracic disc displacement", "M51.24"),
+    ("Herniated disc, Thoracic Spine", "M51.24"),
     ("Thoracic sprain/strain", "S23.3XXA"),
     ("Thoracic muscle spasm", "M62.830"),
     ("Thoracic radiculopathy", "M54.14"),
@@ -75,7 +75,7 @@ DX_LIST: list[tuple[str, str]] = [
     
 
     # Lumbar / SI
-    ("Lumbar disc displacement", "M51.26"),
+    ("Herniated disc, Lumbar Spine", "M51.26"),
     ("Lumbar sprain/strain", "S33.5XXA"),
     ("Lumbar radiculopathy", "M54.16"),
     ("Lumbar muscle spasm", "M62.830"),
